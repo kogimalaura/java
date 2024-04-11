27093020163fc27ec0aa23b09cfbb4f1b290530a
kogimalaura <149600432+kogimalaura@users.noreply.github.com>
2024-04-11 16:37:26 -0300

    y

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const dataAtual= new Date();
-let dataNiver= prompt ("Data do proximo niver:");
-dataNiver= new Date (dataNiver+ "T23:59:59");
+const dataAtual = new Date();
+let dataNiver = prompt ("Data do proximo niver:");
+dataNiver = new Date (dataNiver + "T23:59:59");
